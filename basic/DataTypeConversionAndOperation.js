c7ff337042a547dcb2f4cacb6d630bd809a33069
debugtheprogram <shoaibakhthar883@gmail.com>
2024-04-24 16:06:01 +0530

    Data type conversion and operations

---
+++ basic/DataTypeConversionAndOperation.js
@@ -0,0 +1,39 @@
+let userId = 102040;
+
+let userInput = "123aa";
+
+console.log(typeof userInput); // String
+
+
+/** conversion */
+
+let convertUSerInput = Number(userInput);
+
+console.log(typeof convertUSerInput); // number
+
+//  but check the convert value 
+
+console.log(convertUSerInput); // NaN
+
+/**
+ * "23" => 23
+ * "23xyz" => NaN
+ * true => 1;
+ * false => 0;
+ * 
+ * 
+ * 1 => true;
+ * 0 => false;
+ * "" => false;
+ * "shoaib" => true;
+ */
+
+/** conversion of different dataTypr
+ * 
+ * Number();
+ * String();
+ * Boolean();
+ */
+
+
+//************** OPERATIONS ***************** */
