ed5ae71aac4543c6c385782fe4c0fec6b6746e31
debugtheprogram <shoaibakhthar883@gmail.com>
2024-04-24 16:04:36 +0530

    data types in js

---
+++ basic/DataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) --> we are using node js not a browser
+
+console.log(3
++ 3);// this is not recommended way to write a code 
+
+
+let name = "shoaib";
+let age = 23;
+
+let isLoggedIn = false;
+
+
+/** PRIMITIVE DATA TYPE */
+// number => 2^53
+// bigInt => is used in large scale project
+//String => "shoaib"
+// boolean => true or false
+//null => it a standalone value 
+// undefined => it was decalred not initialize or assign to a variable in a code
+// symbol => unique mostly used in react...
+
+/**OBJECT */
+//typeof is used to check the type of a data type
+
+console.log(typeof age); //  number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
