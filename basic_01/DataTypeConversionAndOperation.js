6cb84d048ba84351de10c2aba9ab2d74ad4e7cdf
debugtheprogram <shoaibakhthar883@gmail.com>
2024-05-08 13:19:05 +0530

    operations in javascript

--- basic_01/DataTypeConversionAndOperation.js
+++ basic_01/DataTypeConversionAndOperation.js
@@ -2,18 +2,18 @@ let userId = 102040;
 
 let userInput = "123aa";
 
-console.log(typeof userInput); // String
+//console.log(typeof userInput); // String
 
 
 /** conversion */
 
 let convertUSerInput = Number(userInput);
 
-console.log(typeof convertUSerInput); // number
+//console.log(typeof convertUSerInput); // number
 
 //  but check the convert value 
 
-console.log(convertUSerInput); // NaN
+//console.log(convertUSerInput); // NaN
 
 /**
  * "23" => 23
@@ -37,3 +37,33 @@ console.log(convertUSerInput); // NaN
 
 
 //************** OPERATIONS ***************** */
+
+let value = 3;
+let negValue = -value;
+console.log(negValue); // -3
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "hello";
+let str2 = "shoaib";
+
+let str3 = str1 + str2 ;
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 3); // 123
+console.log(1 + 2 + "3"); // 33
+
+console.log(+true); //--> 1
+console.log(+"");  //--> 0
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); //--> 101
+++gameCounter;
